dc1ac9005f3c38b8e94d3ae31b8a54c7d34d0f57
sgurnam <94956936+sgurnam@users.noreply.github.com>
2022-10-27 19:02:12 +0530

    Homepage Service Cards BG + Project Details WO Errors 3

--- components/listingSections/FiltersArea.js
+++ components/listingSections/FiltersArea.js
@@ -29,7 +29,7 @@ const FiltersArea = (props) => {
     },
   ];
   return (
-    <div className="filter-area" onClick={changeStateHandler}>
+    <div className="filter-area">
       {filterListing.map((filter, index) => (
         <FilterAccordion
           key={`filterrListing--${index}`}
